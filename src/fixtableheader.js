635cc7dacf1bf665e6fbfef48e0506a29f4aad82
goobeer <goobeer@sina.cn>
2017-10-17 10:19:47 +0800

    Add files via upload
    1、支持单表头
    2、支持多表头及复杂表头：对应的thead下的th width 自动计算

--- src/fixtableheader.js
+++ src/fixtableheader.js
@@ -14,17 +14,77 @@
                 }
             }
 
-            var headerContainer = $(option.containerSelector + ' table tr:first');
-            
-            var headerItems = headerContainer.children();
-            var lastContainer = $(option.containerSelector + ' table tr:last');
-			var firstRowContainer=$(option.containerSelector + ' table tr:nth(1)');
-			headerContainer.css({ 'position': 'fixed', 'top': (firstRowContainer.offset().top-firstRowContainer.height()-1)+'px' });
-            var tds = lastContainer.children();
-            headerContainer.css(option.tabHeaderCss);
-            tds.each(function (i, e) {
-                $(headerItems.get(i)).css({ 'width': $(e).css('width') });
-            });
+            var headerContainer = $(option.containerSelector + ' table thead');
+            var headerItems = headerContainer.children();//tr
+			var lastContainer = $(option.containerSelector + ' table tbody tr:last');
+			
+			if(headerItems.length>1){//多表头
+				var rows=[];
+				headerItems.each(function(i,e){
+					var i=rows.length;
+					var row=$(e).children('th').map(function(ci,ce){
+						var cell=$(ce);
+						var y=cell.attr('rowspan'),x=cell.attr('colspan');
+						y=y?parseInt(y):1;
+						x=x?parseInt(x):1;
+						return {cell:cell,y:y,x:x,id:i+'_'+ci};
+					});
+					rows.push(row);
+				});
+
+				//填充
+				for(var i=0;i<rows.length;i++){
+					for(var j=0;j<rows[i].length;j++){
+						var cell=rows[i][j];
+						var x=cell.x;
+						cell.x=1;
+						while(x>1){
+							rows[i].splice(j,0,cell);
+							x--;
+						}
+						var y=cell.y;
+						cell.y=1;
+						while(y>1 && (i+y-1)<rows.length){
+							rows[i+y-1].splice(j,0,cell);
+							y--;
+						}
+					}
+				}
+				
+				//去重
+				if(rows.length>0){
+					var lastRow=rows[rows.length-1];
+					var ths=[];
+					for(var i=0;i<lastRow.length;i++){
+						var cell=lastRow[i];
+						var contained=false;
+						for(var j=0;j<ths.length;j++){
+							var th=ths[j];
+							if(th.id==cell.id){
+								contained=true;
+								continue;
+							}
+						}
+						if(!contained){
+							ths.push(cell);
+						}
+					}
+					headerItems = $(ths.map(function(e,i){
+						return e.cell;
+					}));
+				}
+			}else{//单表头
+				headerItems=headerItems.children();
+			}
+
+			headerContainer.css(option.tabHeaderCss);
+			headerContainer.css({ 'position': 'fixed','top':'-1px' });
+
+			var tds = lastContainer.children();
+			tds.each(function (i, e) {
+				$(headerItems.get(i)).css({ 'width': $(e).css('width') });
+			});
+
             $(option.containerSelector + ' table tr:nth(1)').children('td').css({ 'padding-top': headerContainer.css('height') });
 
             $(document).scroll(function (e) {
